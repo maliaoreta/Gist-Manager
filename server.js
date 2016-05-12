2b3983d2cb84e6dd0828207932acee3fde861320
maliaoreta <maliaoreta@gmail.com>
2016-05-12 00:15:03 -1000

    starts to set up OAuth

--- server.js
+++ server.js
@@ -1,1 +1,48 @@
 'use strict'
+
+// import express from 'express';
+// import passport from 'passport';
+// import session from 'express-session';
+// import bodyParser from 'body-parser';
+// import {Strategy as GitHubStrategy} from 'passport-github2'
+// import partials from 'express-partials';
+// import * as CONFIG from './config/config.json';
+const express = require('express');
+const app = express();
+const passport = require('passport');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const GitHubStrategy = require('passport-github2').Strategy;
+const partials = require('express-partials');
+const CONFIG = require('./config/config.json')
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((obj, done) => {
+  done(null, obj);
+});
+
+passport.use(new GitHubStrategy({
+  clientID: CONFIG.github_client_id,
+  clientSecret: CONFIG.github_secret_id,
+  callbackURL: "http://localhost:3000/"
+},
+(accessToken, refreshToken, profile, done) => {
+  process.nextTick(() => {
+    return done(null, profile);
+  });
+}
+));
+
+app.
+
+app.use(partials());
+app.use(session({ secret: 'sdjflskdfj', resave: false, saveUninitialized: false}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.listen(3000, () => {
+  console.log('Listening on 3000!');
+});
